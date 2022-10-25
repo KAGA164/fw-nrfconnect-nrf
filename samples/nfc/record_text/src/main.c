6ce675cae8ecce42de6d57cf7bec0f48dd6afbce
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2022-10-25 17:12:49 +0200

    samples: NFC URL record exercise
    Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>

--- samples/nfc/record_text/src/main.c
+++ samples/nfc/record_text/src/main.c
@@ -8,8 +8,11 @@
 #include <zephyr/sys/reboot.h>
 
 #include <nfc_t2t_lib.h>
-#include <nfc/ndef/msg.h>
-#include <nfc/ndef/text_rec.h>
+
+/* TODO: Include required headers in section below. */
+/* NFC NDEF headers begin: */
+
+/* NFC NDEF headers end */
 
 #include <dk_buttons_and_leds.h>
 
@@ -17,25 +20,6 @@
 #define NDEF_MSG_BUF_SIZE	128
 
 #define NFC_FIELD_LED		DK_LED1
-
-/* Text message in English with its language code. */
-static const uint8_t en_payload[] = {
-	'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'
-};
-static const uint8_t en_code[] = {'e', 'n'};
-
-/* Text message in Norwegian with its language code. */
-static const uint8_t no_payload[] = {
-	'H', 'a', 'l', 'l', 'o', ' ', 'V', 'e', 'r', 'd', 'e', 'n', '!'
-};
-static const uint8_t no_code[] = {'N', 'O'};
-
-/* Text message in Polish with its language code. */
-static const uint8_t pl_payload[] = {
-	'W', 'i', 't', 'a', 'j', ' ', 0xc5, 0x9a, 'w', 'i', 'e', 'c', 'i',
-	'e', '!'
-};
-static const uint8_t pl_code[] = {'P', 'L'};
 
 /* Buffer used to hold an NFC NDEF message. */
 static uint8_t ndef_msg_buf[NDEF_MSG_BUF_SIZE];
@@ -66,67 +50,13 @@ static void nfc_callback(void *context,
 /**
  * @brief Function for encoding the NDEF text message.
  */
-static int welcome_msg_encode(uint8_t *buffer, uint32_t *len)
+static int url_msg_encode(uint8_t *buffer, uint32_t *len)
 {
-	int err;
+	/* TODO: Create URL record which contains the address of the AGH website(https://www.agh.edu.pl) */
+	/* URL record begin: */
 
-	/* Create NFC NDEF text record description in English */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_en_text_rec,
-				      UTF_8,
-				      en_code,
-				      sizeof(en_code),
-				      en_payload,
-				      sizeof(en_payload));
 
-	/* Create NFC NDEF text record description in Norwegian */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_no_text_rec,
-				      UTF_8,
-				      no_code,
-				      sizeof(no_code),
-				      no_payload,
-				      sizeof(no_payload));
-
-	/* Create NFC NDEF text record description in Polish */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_pl_text_rec,
-				      UTF_8,
-				      pl_code,
-				      sizeof(pl_code),
-				      pl_payload,
-				      sizeof(pl_payload));
-
-	/* Create NFC NDEF message description, capacity - MAX_REC_COUNT
-	 * records
-	 */
-	NFC_NDEF_MSG_DEF(nfc_text_msg, MAX_REC_COUNT);
-
-	/* Add text records to NDEF text message */
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_en_text_rec));
-	if (err < 0) {
-		printk("Cannot add first record!\n");
-		return err;
-	}
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_no_text_rec));
-	if (err < 0) {
-		printk("Cannot add second record!\n");
-		return err;
-	}
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_pl_text_rec));
-	if (err < 0) {
-		printk("Cannot add third record!\n");
-		return err;
-	}
-
-	err = nfc_ndef_msg_encode(&NFC_NDEF_MSG(nfc_text_msg),
-				      buffer,
-				      len);
-	if (err < 0) {
-		printk("Cannot encode message!\n");
-	}
-
-	return err;
+	/* URL record end. */
 }
 
 
@@ -150,7 +80,7 @@ int main(void)
 
 
 	/* Encode welcome message */
-	if (welcome_msg_encode(ndef_msg_buf, &len) < 0) {
+	if (url_msg_encode(ndef_msg_buf, &len) < 0) {
 		printk("Cannot encode message!\n");
 		goto fail;
 	}
